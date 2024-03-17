877c0f918f4267c3b538610f3d91db3f3be10fd9
Debian <bitnami@ip-172-26-15-201.us-west-2.compute.internal>
2024-03-17 21:01:54 +0000

    constants changes and package-lock.json changes

--- constants.php
+++ constants.php
@@ -2,6 +2,7 @@
 // Application Constants
 
 const APPLICATION_TITLE = 'SESSFA Move-a-thon Tracker';
-const MOVEMENT_GOAL = 20000;
+const MOVEMENT_GOAL = 60000;
+// const MOVEMENT_GOAL = 40075000;
 const MOVE_A_THON_START_DATE = "2024-01-01";
 const MOVE_A_THON_END_DATE = "2024-06-01";
