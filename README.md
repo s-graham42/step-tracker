40a3efafc27c5bdfcb63e7d79708b0f18f5dbf70
Stephen Graham <stephen.graham@ssaihq.com>
2024-03-30 13:35:57 -0700

    better font import for all pages.  readme changes.

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 StepTracker Readme
 
-https://docs.google.com/document/d/1VBGVqZ8hp98fu2JgRhMTOPX805PeOfWRb9W3wdTaz7o/edit#heading=h.5ndhu5v2rva2
+sessfasteptracker.org
 
-34.218.208.215
+This tiny webapp allows users to add their pedometer steps to the database for each day of
+the SESSFA Move a Thon.  The total steps for all kid users and all adult users are then
+shown on the results page.
+
+Move a thon start date (first day a user can add steps for): April 19th 2024
+Move a thon end date (last date a user can add steps for): May 3rd 2024
--- index.php
+++ index.php
@@ -16,7 +16,9 @@
     <title><?= APPLICATION_TITLE; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./css/style.css">
 
 </head>
