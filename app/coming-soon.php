d97650b408d5e1becaaf9f8cf1ad1b05d762fd9b
Stephen Graham <stephen.graham@ssaihq.com>
2024-03-30 12:50:46 -0700

    remove footer

--- app/coming-soon.php
+++ app/coming-soon.php
@@ -39,7 +39,6 @@
             and see if kids can move farther than adults.</p>
     </div>
 </main>
-<footer style="width:100%; height:100px;"></footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
