ec9e39dfadd6ef26ae4112a18e00d09940d8b8e6
Stephen Graham <stephen.graham@ssaihq.com>
2024-04-16 21:34:34 -0700

    adding google analytics

--- index.php
+++ index.php
@@ -11,6 +11,15 @@
 <html lang="en">
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-EDLRSZKP5W"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-EDLRSZKP5W');
+    </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= APPLICATION_TITLE; ?></title>
